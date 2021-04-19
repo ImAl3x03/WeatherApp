6f5185f301280cf2ad1d402918933fbf04a2dede
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-04-19 20:55:59 +0200

    Added hidden API Key

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
 
 namespace WeatherApp.Pages
 {
@@ -12,10 +13,27 @@ namespace WeatherApp.Pages
     {
         private string _url = "http://api.weatherapi.com/v1/current.json?key=";
 
+        public IndexModel()
+        {
+            string key;
+            
+            try
+            {
+                var read = new StreamReader("API.txt");
+                key = read.ReadLine();
+            }
+            catch (Exception)
+            {
+                key = Environment.GetEnvironmentVariable("ApiKey");
+            }
+
+            _url += key;
+        }
+        
         public async Task OnPostSearch(string city)
         {
             var req = new HttpClient();
-            _url += "&q=" + city + "&aqi=no";
+            _url += $"&q={city}&aqi=no";
             var result = await req.GetStringAsync(_url);
         }
     }
