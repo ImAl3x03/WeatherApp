cbd4fde0a8f793ca0e80aae4f715eae8ffb94422
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-04-19 19:47:26 +0200

    Added Request to see weather

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,9 +10,13 @@ namespace WeatherApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private string _url = "http://api.weatherapi.com/v1/current.json?key=";
+
         public async Task OnPostSearch(string city)
         {
-            await Task.Delay(500);
+            var req = new HttpClient();
+            _url += "&q=" + city + "&aqi=no";
+            var result = await req.GetStringAsync(_url);
         }
     }
 }
