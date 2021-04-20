b21f72d96b2c5ae78f812723a86b057f515c7e00
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-04-20 10:14:26 +0200

    Modified error layout

--- wwwroot/JavaScript/image.js
+++ wwwroot/JavaScript/image.js
@@ -1,8 +1,13 @@
-﻿let image = document.querySelector("div.weather img");
+﻿let image = document.querySelectorAll("main img");
+let text = document.querySelector("div.weather p ");
 
-if(image.src === "") {
-    image.classList.add("hide");
+if(text.innerHTML === "Please insert a valid city") {
+    for (let img of image) {
+        img.classList.add("hide");
+    }
 }
 else {
-    image.classList.remove("hide");
+    for(let img of image) {
+        img.classList.remove("hide");
+    }
 }
