3271f26c7af650fd0b6fa19b7f591ed4599bedfc
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-04-20 09:09:41 +0200

    Added some other information from JSON

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,14 +9,31 @@ namespace WeatherApp.Pages
 {
     public class IndexModel : PageModel
     {
+	/*
+	* TODO
+	* Modify Info from variable to prop
+	*/
+
+	/*
+	* TODO
+	* Take other information from JSON
+	* TODO
+	* Modify Information class
+	* TODO
+	* Show the new Information
+	* TODO
+	* Remove initialize Information from ctor
+	*/
+
         private string _url = "http://api.weatherapi.com/v1/current.json?key=";
-        public Information Info;
+        public Information Info { get; private init; }
+        public Location Location { get; private set; }
         
         private string _result = "";
 
         public IndexModel()
         {
-            Info = new Information {Icon = null};
+            Info = new Information();
             string key;
             
             try
@@ -34,29 +51,38 @@ namespace WeatherApp.Pages
         
         public async Task OnPostSearch(string city)
         {
-            var req = new HttpClient();
-            _url += $"&q={city}&aqi=no";
-            try
+            using (var req = new HttpClient())
             {
-                _result = await req.GetStringAsync(_url);
-            }
-            catch (HttpRequestException)
-            {
-                Info.Text = "Please insert a valid city";
-                return;
-            }
-            catch (Exception)
-            {
-                Info.Text = "Error!";
-                return;
+                _url += $"&q={city}&aqi=no";
+                try
+                {
+                    _result = await req.GetStringAsync(_url);
+                }
+                catch (HttpRequestException)
+                {
+                    Info.Text = "Please insert a valid city";
+                    return;
+                }
+                catch (Exception)
+                {
+                    Info.Text = "Error!";
+                    return;
+                }
             }
 
             var deserialized = JObject.Parse(_result);
             try
             {
-                Info = deserialized["current"]?["condition"]?.ToObject<Information>();
+                Info.TempC = deserialized["current"]?["temp_c"]?.ToObject<double>();
+                Info.Text = deserialized["current"]?["condition"]?["text"]?.ToString();
+                Info.Icon = deserialized["current"]?["condition"]?["icon"]?.ToString();
+                Location = deserialized["location"]?.ToObject<Location>();
             }
             catch (NullReferenceException)
+            {
+                RedirectToPage("Index");
+            }
+            catch (InvalidOperationException)
             {
                 RedirectToPage("Index");
             }
@@ -66,6 +92,26 @@ namespace WeatherApp.Pages
     public class Information
     {
         public string Text { get; set; }
-        public string Icon { get; init; }
+        public string Icon { get; set; }
+        public double? TempC { get; set; }
+    }
+
+    public class Location
+    {
+        public Location(string name, string region, string country)
+        {
+            Name = name;
+            Region = region;
+            Country = country;
+        }
+
+        private string Name { get; set; }
+        private string Region { get; set; }
+        private string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Region}, {Country}";
+        }
     }
 }
