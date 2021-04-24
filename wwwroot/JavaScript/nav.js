4b531ba3c1cfd08bafd2dd2c2e4cb7f785cfb6b8
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-04-24 21:01:27 +0200

    Navbar animation

---
+++ wwwroot/JavaScript/nav.js
@@ -0,0 +1,30 @@
+﻿const navbarElement = document.querySelector("div.wrap header nav");
+let number = 0;
+let open = false;
+let id = undefined;
+
+function anim() {
+    id = setInterval(navAnimation, 10);
+    console.log("function called");
+}
+
+function navAnimation() {
+    if(open) {
+        number += 2;
+        navbarElement.style.left = number + 'px';
+    }
+    else {
+        number -= 2;
+        navbarElement.style.left = number + 'px';
+    }
+
+    if(parseInt(navbarElement.style.left) === -100) {
+        clearInterval(id);
+        open = true;
+    }
+
+    if(parseInt(navbarElement.style.left) === 0) {
+        clearInterval(id);
+        open = false;
+    }
+}
